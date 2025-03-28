0edbbf2740fc193b6449fb8223d3a5ae2b0ebf76
vsbrall143 <vsbrall143@gmail.com>
2025-03-28 14:35:38 +0530

    deploymentt

--- util/database.js
+++ util/database.js
@@ -1,11 +1,11 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'Xmachin@123', {
-dialect: 'mysql',
-host: 'localhost'
-});
+// const sequelize = new Sequelize('node-complete', 'root', 'Xmachin@123', {
+// dialect: 'mysql',
+// host: 'localhost'
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
  
 
 
