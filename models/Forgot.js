dea13f69b58af2fc1401b70ff60c74d3dedb7966
vikas sharma <93755781+vsbrall143@users.noreply.github.com>
2025-03-28 23:48:11 +0530

    Update Forgot.js

--- models/Forgot.js
+++ models/Forgot.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const forgotPasswordSchema = new mongoose.Schema({
     id: { type: String, required: true, unique: true },
     isActive: { type: Boolean, default: true },
- 
+ signupEmail: { type: String, required: true }
 });
 
 const ForgotPasswordRequest = mongoose.model('ForgotPasswordRequest', forgotPasswordSchema);
